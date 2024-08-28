762602a8a08132f8a6c910c1fe8af4452b367b1c
Pianlerdcdtihub <6629011007@cdti.ac.th>
2024-08-28 16:39:58 +0700

    update

---
+++ myweb/blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.template import loader
+
+def blog(request):
+    template = loader.get_template('blog.html')
+    return HttpResponse(template.render())
